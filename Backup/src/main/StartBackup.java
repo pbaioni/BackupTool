695c4ce84116669f8e38c3132ec5bb8ae72612b3
paolobaioni <paolobaioni@192.168.0.13>
2020-04-17 21:07:45 +0200

    build exe jar

--- Backup/src/main/StartBackup.java
+++ Backup/src/main/StartBackup.java
@@ -8,33 +8,40 @@ import main.action.FileTree;
 
 public class StartBackup {
 
-	//Copy properties
+	// Copy properties
 	private static String rootFolderPath = "/Users/paolobaioni/Documents/Music";
-	
-	private static String[] extensionsToCopy = { };
 
-	//Backup properties
-	private static String syncSource = "/Users/paolobaioni/TestSource";
-	//private static String syncSource = "/Users/paolobaioni/Documents/Pictures";
+	private static String[] extensionsToCopy = {};
 
-	private static String syncDest = "/Users/paolobaioni/TestDest";
-	//private static String syncDest = "/Volumes/Paolo_backup/Documents/Pictures";
-	
+	// Backup properties
+	private static String testSyncSource = "/Users/paolobaioni/TestSource";
+	private static String syncSource = "/Users/paolobaioni/Documents/";
+
+	private static String testSyncDest = "/Users/paolobaioni/TestDest";
+	private static String syncDest = "/Volumes/Paolo_backup/Documents/";
 
 	public static void main(String[] args) throws IOException {
-		
-		Copy copy = new Copy(rootFolderPath);
-		for(String extension : extensionsToCopy) {
-			copy.copyFilesByExtension(extension);
+
+		boolean isTest = true;
+
+		for (String arg : args) {
+			isTest = false;
+			if (!arg.equals("")) {
+				Backup backup = new Backup(syncSource + arg, syncDest + arg);
+				backup.synchronizeFolders();
+			}
 		}
-		
-		Backup backup = new Backup(syncSource, syncDest);
-		backup.synchronizeFolders();
+
+		if (isTest) {
+			Copy copy = new Copy(rootFolderPath);
+			for (String extension : extensionsToCopy) {
+				copy.copyFilesByExtension(extension);
+			}
+
+			Backup backup = new Backup(testSyncSource, testSyncDest);
+			backup.synchronizeFolders();
+		}
 
 	}
-	
-
-
-
 
 }
